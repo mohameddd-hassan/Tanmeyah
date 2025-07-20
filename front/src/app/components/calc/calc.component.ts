e4ea281569f5bf7a728a8deb2a245a1718ac8498
Your Name <your@email.com>
2025-07-20 12:44:18 +0300

    updated calculate  section calculate

--- front/src/app/components/calc/calc.component.ts
+++ front/src/app/components/calc/calc.component.ts
@@ -10,6 +10,9 @@ import { Component } from '@angular/core';
 export class CalcComponent {
   value1 = 0;
   value2 = 0;
+  result = 0;
+  message = '';
+  
 
   onSlider1Change(event: Event) {
     const input = event.target as HTMLInputElement;
@@ -19,5 +22,15 @@ export class CalcComponent {
   onSlider2Change(event: Event) {
     const input = event.target as HTMLInputElement;
     this.value2 = +input.value;
+  }
+
+  calculateLoan() {
+    if (this.value2 > 0) {
+      this.result = this.value1 / this.value2;
+      this.message = 'This calculation is for illustrative purposes only and does not constitute a final offer or cash amounts. The actual amount or installment may vary based on the terms and conditions.';
+    } else {
+   
+      this.message = 'Please enter a valid number of installments.';
+    }
   }
 }
