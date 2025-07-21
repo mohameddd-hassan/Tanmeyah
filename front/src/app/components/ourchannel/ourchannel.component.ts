df9836da099a3ec5fe295a2c05c6ee40bf1699c1
Your Name <your@email.com>
2025-07-21 16:18:51 +0300

    2 translated section

--- front/src/app/components/ourchannel/ourchannel.component.ts
+++ front/src/app/components/ourchannel/ourchannel.component.ts
@@ -1,18 +1,40 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-ourchannel',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, TranslateModule],
   templateUrl: './ourchannel.component.html',
-  styleUrl: './ourchannel.component.css'
+  styleUrl: './ourchannel.component.css',
 })
 export class OurchannelComponent {
-channels=[
-  {img:'../../../assets/images/channels/location.svg',title:'Branch Locator And',p:'ATMS' },
-  {img:'../../../assets/images/channels/phone.svg',title:'Hot Line',p:'16164' },
-  {img:'../../../assets/images/channels/whats.svg',title:'Our Whatsapp',p:' Abo El Seba3' },
-  {img:'../../../assets/images/channels/bag.svg',title:'Place & follow your request',p:' Easy & Quick Loans' },
-  {img:'../../../assets/images/channels/hand.svg',title:'the E payment partners',p:'Convenient Payments' },
-]
+  channels = [
+    {
+      img: '../../../assets/images/channels/location.svg',
+      title: 'ourChannels.channels.0.title',
+      p: 'ourChannels.channels.0.p',
+    },
+    {
+      img: '../../../assets/images/channels/phone.svg',
+      title: 'ourChannels.channels.1.title',
+      p: 'ourChannels.channels.1.p',
+    },
+    {
+      img: '../../../assets/images/channels/whats.svg',
+      title: 'ourChannels.channels.2.title',
+      p: 'ourChannels.channels.2.p',
+    },
+    {
+      img: '../../../assets/images/channels/bag.svg',
+      title: 'ourChannels.channels.3.title',
+      p: 'ourChannels.channels.3.p',
+    },
+    {
+      img: '../../../assets/images/channels/hand.svg',
+      title: 'ourChannels.channels.4.title',
+      p: 'ourChannels.channels.4.p',
+    },
+  ];
 }
